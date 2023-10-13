29085fd7d391f52c1aa6a536b5bcfdaffe951c5d
ValenNanni <valbelnan@gmail.com>
2023-10-13 18:46:56 -0300

    Index

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecos del Pasado</title>
+    <link rel="shortcut icon" href="img/favicon-16x16.png" type="image/x-icon">
+    <link rel="stylesheet" href="estilosindex.css">
+</head>
+
+
+<body>
+    <header>
+
+        <section class="textos-header">
+        <h1>Biblioteca Ecos del Pasado</h1>
+        </section>
+
+        <div class= "wave" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C150.00,150.00 349.20,-50.00 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #fff;"></path></svg></div>
+
+    </header>
+    
+    </form>
+
+    <section id="login">
+        <h2>Iniciar Sesión del Bibliotecario</h2>
+        <form id="loginForm" action="panel_bibliotecario.php" method="post">
+            <label for="usuario">Usuario:</label>
+            <input type="text" id="usuario" name="usuario" required>
+            <br>
+            <label for="contrasena">Contraseña:</label>
+            <input type="password" id="contrasena" name="contrasena" required>
+            <br>
+            <button type="submit">Iniciar Sesión</button>
+        </form>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Biblioteca Ecos del Pasado - Todos los derechos reservados</p>
+    </footer>
+</body>
+</div>
+</html>
