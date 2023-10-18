7beb23253a46839f392330867ec5462d4c42bd9f
camicauzzi <143765192+camicauzzi@users.noreply.github.com>
2023-10-18 15:46:35 -0300

    Update scripbiblioteca.sql

--- scripbiblioteca.sql
+++ scripbiblioteca.sql
@@ -15,16 +15,13 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema biblioteca2
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `biblioteca2` DEFAULT CHARACTER SET utf8mb3 ;
--- -----------------------------------------------------
--- Schema biblioteca
--- -----------------------------------------------------
 USE `biblioteca2` ;
 
 -- -----------------------------------------------------
 -- Table `biblioteca2`.`autores`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `biblioteca2`.`autores` (
-  `id_autor` INT NOT NULL,
+  `id_autor` INT NOT NULL AUTO_INCREMENT,
   `nombre_autor` VARCHAR(45) NOT NULL,
   `apellido_autor` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id_autor`))
@@ -36,9 +33,9 @@ DEFAULT CHARACTER SET = utf8mb3;
 -- Table `biblioteca2`.`generos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `biblioteca2`.`generos` (
-  `id_genero` INT NOT NULL,
-  `nombre_genero` VARCHAR(45) NULL DEFAULT NULL,
-  `descripcion_genero` VARCHAR(45) NULL DEFAULT NULL,
+  `id_genero` INT NOT NULL AUTO_INCREMENT,
+  `nombre_genero` VARCHAR(45) NOT NULL,
+  `descripcion_genero` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id_genero`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
@@ -48,17 +45,16 @@ DEFAULT CHARACTER SET = utf8mb3;
 -- Table `biblioteca2`.`personas`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `biblioteca2`.`personas` (
-  `id_persona` INT NOT NULL,
-  `tipo_persona` ENUM('Usuario', 'Empleado') NOT NULL,
+  `id_persona` INT NOT NULL AUTO_INCREMENT,
   `nombre_persona` VARCHAR(45) NOT NULL,
   `apellido_persona` VARCHAR(45) NOT NULL,
   `dni_persona` VARCHAR(45) NOT NULL,
   `telefono_persona` VARCHAR(45) NOT NULL,
-  `direccion_empleado` VARCHAR(45) NULL DEFAULT NULL,
-  `email_empleado` VARCHAR(45) NULL DEFAULT NULL,
-  `fecha_contratacion` DATETIME NULL DEFAULT NULL,
-  `usuario_empleado` VARCHAR(45) NULL DEFAULT NULL,
-  `clave_empleado` VARCHAR(250) NULL DEFAULT NULL,
+  `direccion_empleado` VARCHAR(45) NOT NULL,
+  `email_empleado` VARCHAR(45) NOT NULL,
+  `fecha_contratacion` DATETIME NOT NULL,
+  `usuario_empleado` VARCHAR(45) NOT NULL,
+  `clave_empleado` VARCHAR(250) NOT NULL,
   PRIMARY KEY (`id_persona`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
@@ -68,46 +64,30 @@ DEFAULT CHARACTER SET = utf8mb3;
 -- Table `biblioteca2`.`libros`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `biblioteca2`.`libros` (
-  `id_libro` INT NOT NULL,
+  `id_libro` INT NOT NULL AUTO_INCREMENT,
   `nombre_libro` VARCHAR(45) NOT NULL,
   `anio_publicacion` VARCHAR(45) NOT NULL,
   `copias_disponibles` VARCHAR(45) NOT NULL,
-  `id_genero` INT NOT NULL,
-  `id_autor` INT NOT NULL,
-  `personas_id_persona` INT NOT NULL,
-  `generos_id_genero` INT NOT NULL,
-  `autores_id_autor` INT NOT NULL,
-  PRIMARY KEY (`id_libro`, `id_genero`, `id_autor`, `personas_id_persona`, `generos_id_genero`, `autores_id_autor`),
-  INDEX `fk_libros_generos1_idx` (`id_genero` ASC) VISIBLE,
-  INDEX `fk_libros_autores1_idx` (`id_autor` ASC) VISIBLE,
-  INDEX `fk_libros_personas1_idx` (`personas_id_persona` ASC) VISIBLE,
-  INDEX `fk_libros_generos2_idx` (`generos_id_genero` ASC) VISIBLE,
-  INDEX `fk_libros_autores2_idx` (`autores_id_autor` ASC) VISIBLE,
-  CONSTRAINT `fk_libros_autores1`
-    FOREIGN KEY (`id_autor`)
-    REFERENCES `mydb`.`autores` (`id_autor`),
-  CONSTRAINT `fk_libros_generos1`
-    FOREIGN KEY (`id_genero`)
-    REFERENCES `mydb`.`generos` (`id_genero`),
-  CONSTRAINT `fk_libros_personas1`
-    FOREIGN KEY (`personas_id_persona`)
-    REFERENCES `biblioteca2`.`personas` (`id_persona`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_libros_generos2`
-    FOREIGN KEY (`generos_id_genero`)
-    REFERENCES `biblioteca2`.`generos` (`id_genero`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  `id_persona` INT NOT NULL ,
+  `id_genero` INT NOT NULL ,
+  `id_autor` INT NOT NULL ,
+  PRIMARY KEY (`id_libro`, `id_persona`, `id_genero`, `id_autor`),
+  INDEX `fk_libros_personas1_idx` (`id_persona` ASC) ,
+  INDEX `fk_libros_generos2_idx` (`id_genero` ASC) ,
+  INDEX `fk_libros_autores2_idx` (`id_autor` ASC) ,
   CONSTRAINT `fk_libros_autores2`
-    FOREIGN KEY (`autores_id_autor`)
-    REFERENCES `biblioteca2`.`autores` (`id_autor`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    FOREIGN KEY (`id_autor`)
+    REFERENCES `biblioteca2`.`autores` (`id_autor`),
+  CONSTRAINT `fk_libros_generos2`
+    FOREIGN KEY (`id_genero`)
+    REFERENCES `biblioteca2`.`generos` (`id_genero`),
+  CONSTRAINT `fk_libros_personas1`
+    FOREIGN KEY (`id_persona`)
+    REFERENCES `biblioteca2`.`personas` (`id_persona`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+
