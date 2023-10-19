bd2b90c98dd18cf90a9f36301524148d69b0ff4a
camicauzzi <143765192+camicauzzi@users.noreply.github.com>
2023-10-19 18:06:51 -0300

    Update scripbiblioteca.sql

--- scripbiblioteca.sql
+++ scripbiblioteca.sql
@@ -14,80 +14,46 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema biblioteca2
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `biblioteca2` DEFAULT CHARACTER SET utf8mb3 ;
+CREATE SCHEMA IF NOT EXISTS `biblioteca2` DEFAULT CHARACTER SET utf8 ;
 USE `biblioteca2` ;
 
 -- -----------------------------------------------------
--- Table `biblioteca2`.`autores`
+-- Table `biblioteca2`.`libros`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `biblioteca2`.`autores` (
-  `id_autor` INT NOT NULL AUTO_INCREMENT,
-  `nombre_autor` VARCHAR(45) NOT NULL,
-  `apellido_autor` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_autor`))
+CREATE TABLE IF NOT EXISTS `biblioteca2`.`libros` (
+  `id_libro` INT(11) NOT NULL AUTO_INCREMENT,
+  `nombre_libro` VARCHAR(45) NOT NULL,
+  `anio_publicacion` VARCHAR(45) NOT NULL,
+  `copias_disponibles` VARCHAR(45) NOT NULL,
+  `genero` VARCHAR(45) NOT NULL,
+  `autor` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_libro`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `biblioteca2`.`generos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `biblioteca2`.`generos` (
-  `id_genero` INT NOT NULL AUTO_INCREMENT,
-  `nombre_genero` VARCHAR(45) NOT NULL,
-  `descripcion_genero` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_genero`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `biblioteca2`.`personas`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `biblioteca2`.`personas` (
-  `id_persona` INT NOT NULL AUTO_INCREMENT,
+  `id_persona` INT(11) NOT NULL AUTO_INCREMENT,
+  `tipo_persona` ENUM('Usuario', 'Empleado') NOT NULL,
   `nombre_persona` VARCHAR(45) NOT NULL,
   `apellido_persona` VARCHAR(45) NOT NULL,
   `dni_persona` VARCHAR(45) NOT NULL,
   `telefono_persona` VARCHAR(45) NOT NULL,
-  `direccion_empleado` VARCHAR(45) NOT NULL,
-  `email_empleado` VARCHAR(45) NOT NULL,
-  `fecha_contratacion` DATETIME NOT NULL,
-  `usuario_empleado` VARCHAR(45) NOT NULL,
-  `clave_empleado` VARCHAR(250) NOT NULL,
+  `direccion_empleado` VARCHAR(45) NULL DEFAULT NULL,
+  `email_empleado` VARCHAR(45) NULL DEFAULT NULL,
+  `fecha_contratacion` DATETIME NULL DEFAULT NULL,
+  `usuario_empleado` VARCHAR(45) NULL DEFAULT NULL,
+  `clave_empleado` VARCHAR(250) NULL DEFAULT NULL,
   PRIMARY KEY (`id_persona`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
 
 
--- -----------------------------------------------------
--- Table `biblioteca2`.`libros`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `biblioteca2`.`libros` (
-  `id_libro` INT NOT NULL AUTO_INCREMENT,
-  `nombre_libro` VARCHAR(45) NOT NULL,
-  `anio_publicacion` VARCHAR(45) NOT NULL,
-  `copias_disponibles` VARCHAR(45) NOT NULL,
-  `id_persona` INT NOT NULL ,
-  `id_genero` INT NOT NULL ,
-  `id_autor` INT NOT NULL ,
-  PRIMARY KEY (`id_libro`, `id_persona`, `id_genero`, `id_autor`),
-  INDEX `fk_libros_personas1_idx` (`id_persona` ASC) ,
-  INDEX `fk_libros_generos2_idx` (`id_genero` ASC) ,
-  INDEX `fk_libros_autores2_idx` (`id_autor` ASC) ,
-  CONSTRAINT `fk_libros_autores2`
-    FOREIGN KEY (`id_autor`)
-    REFERENCES `biblioteca2`.`autores` (`id_autor`),
-  CONSTRAINT `fk_libros_generos2`
-    FOREIGN KEY (`id_genero`)
-    REFERENCES `biblioteca2`.`generos` (`id_genero`),
-  CONSTRAINT `fk_libros_personas1`
-    FOREIGN KEY (`id_persona`)
-    REFERENCES `biblioteca2`.`personas` (`id_persona`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
-
-
-
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
