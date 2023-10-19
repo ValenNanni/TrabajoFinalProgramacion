f9d4e2b243314be0970f7b6751028ea905788eaf
ValenNanni <106619774+ValenNanni@users.noreply.github.com>
2023-10-19 18:14:59 -0300

    Update crear_usuario.php

--- crear_usuario.php
+++ crear_usuario.php
@@ -1,16 +1,16 @@
 <?php
 require_once 'clases/ControlSesion.php';
 
-if (isset($_POST['usuario']) && isset($_POST['clave'])) {
+if (isset($_POST['usuario_empleado']) && isset($_POST['clave_empleado'])) {
     $cs = new ControlSesion();
-    $result = $cs->create($_POST['usuario'], $_POST['nombre'],
-                          $_POST['apellido'], $_POST['clave']);
+    $result = $cs->create($_POST['nombre_persona'], $_POST['apellido_persona'], $_POST['dni_persona'], $_POST['telefono_persona'],  
+    $_POST['direccion_empleado'], $_POST['email_empleado'], $_POST['fecha_contratacion'], $_POST['usuario_empleado'], $_POST['clave_empleado']);
     if( $result[0] === true ) {
         $redirigir = 'panel_bibliotecario.php?mensaje='.$result[1];
     } else {
-        $redirigir = 'crear_usuario.php?mensaje='.$result[1];
+        $redirigir = 'cargar_nuevo_usuario.php?mensaje='.$result[1];
     }
 } else {
-    $redirigir = 'crear_usuario.php?mensaje=Elegir usuario y clave';
+    $redirigir = 'cargar_nuevo_usuario.php?mensaje=Hay que elegir usuario y clave';
 }
 header('Location: ' . $redirigir);
