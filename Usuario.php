d6fb696b5d4754a0d4649d1e7374b7c72aab1a52
ValenNanni <106619774+ValenNanni@users.noreply.github.com>
2023-10-19 18:10:49 -0300

    Update Usuario.php

--- Usuario.php
+++ Usuario.php
@@ -3,17 +3,17 @@
 class Usuario
 {
     protected $id_persona;
-    public $nombre_persona;
-    public $apellido_persona;
-    public $dni_persona;
-    public $telefono_persona;
-    public $direccion_empleado;
-    public $email_empleado;
-    public $fecha_contratacion;
-    public $usuario_empleado;
+    protected $nombre_persona;
+    protected $apellido_persona;
+    protected $dni_persona;
+    protected $telefono_persona;
+    protected $direccion_empleado;
+    protected $email_empleado;
+    protected $fecha_contratacion;
+    protected $usuario_empleado;
     protected $clave_empleado;
 
-    public function __construct($id_persona=null, $nombre_persona, $apellido_persona, $dni_persona, $telefono_persona, $direccion_empleado, $fecha_contratacion, $email_empleado, $usuario_empleado, $clave_empleado)
+    public function __construct($nombre_persona, $apellido_persona, $dni_persona, $telefono_persona, $direccion_empleado, $email_empleado, $fecha_contratacion, $usuario_empleado, $clave_empleado, $id_persona=null)
     {
         $this->id_persona = $id_persona;
         $this->nombre_persona = $nombre_persona;
@@ -27,10 +27,7 @@ class Usuario
         $this->clave_empleado = $clave_empleado;
     }
 
-    public function getNombreApellido()
-    {
-        return "$this->nombre_persona $this->apellido_persona";
-    }
+   
     public function setId($id)
     {
         $this->id_persona = $id_persona;
@@ -39,6 +36,53 @@ class Usuario
     {
         return $this->id_persona;
     }
+
+    public function getNombre()
+    {
+        return $this->nombre_persona;
+    }
+
+    public function getApellido()
+    {
+        return $this->apellido_persona;
+    }
+
+    public function getDni()
+    {
+        return $this->dni_persona;
+    }
+
+    public function getTelefono()
+    {
+        return $this->telefono_persona;
+    }
+
+    public function getDireccion()
+    {
+        return $this->direccion_empleado;
+    }
+
+    public function getEmail()
+    {
+        return $this->email_empleado;
+    }
+
+    public function getFechaContratacion()
+    {
+        return $this->fecha_contratacion;
+    }
+
+    public function getUsuario()
+    {
+        return $this->usuario_empleado;
+    }
+
+
+    public function getNombreApellido()
+    {
+        return "$this->nombre_persona $this->apellido_persona";
+    }
+
     public function setDatos($nombre_persona, $apellido_persona, $usuario_empleado)
     {
         $this->nombre_persona = $nombre_persona;
