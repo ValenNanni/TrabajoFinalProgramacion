320d6fab8f23e4ee509e74a80c1eb57bd5e9a342
valennanni <valbelnan@gmail.com>
2023-10-20 16:46:20 -0300

    Cambios en nombres de variables y funciones nuevas agregadas

--- Libro.php
+++ Libro.php
@@ -6,17 +6,39 @@ class Libro
     public $nombre_libro;
     public $anio_publicacion;
     public $copias_disponibles;
-    protected $id_genero;
-    protected $id_autor;
+    public $autor;
+    public $genero;
+    
 
-    public function __construct($id_libro=null, $nombre_libro, $anio_publicacion, $copias_disponibles, $id_genero=null, $id_autor=null)
+    public function __construct($nombre_libro, $anio_publicacion, $copias_disponibles, $genero, $autor, $id_libro=null)
     {
         $this->id_libro = $id_libro;
         $this->nombre_libro = $nombre_libro;
         $this->anio_publicacion = $anio_publicacion;
         $this->copias_disponibles = $copias_disponibles;
-        $this->id_genero = $id_genero;
-        $this->id_autor = $id_autor;
+        $this->autor = $autor;
+        $this->genero = $genero;
+        
+    }
+
+    public function getNombreLibro() {
+        return $this->nombre_libro;
+    }
+
+    public function getAnioPublicacion() {
+        return $this->anio_publicacion;
+    }
+
+    public function getCopiasDisponibles() {
+        return $this->copias_disponibles;
+    }
+
+    public function getAutor() {
+        return $this->autor;
+    }
+
+    public function getGenero() {
+        return $this->genero;
     }
 
     public function setId($id)
@@ -27,15 +49,15 @@ class Libro
     {
         return $this->id_libro;
     }
-    public function setDatos($nombre_libro, $id_genero, $id_autor)
+    public function setDatos($nombre_libro, $genero, $autor)
     {
         $this->nombre_libro = $nombre_libro;
-        $this->id_genero = $id_genero;
-        $this->id_autor = $id_autor;
+        $this->genero = $genero;
+        $this->autor = $autor;
     }
 
     public function getDatos()
     {
-        return "$this->nombre_libro $this->id_genero $this->id_autor";
+        return "$this->nombre_libro $this->genero $this->autor";
     }
 }
